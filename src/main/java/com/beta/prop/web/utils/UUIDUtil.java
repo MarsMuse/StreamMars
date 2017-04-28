52bf878bf947c38dbd1729ea19b9635acd05f632
betaIsMars <13889146832@163.com>
2017-04-28 11:55:59 +0800

    新增获取到UUID工具类

---
+++ src/main/java/com/beta/prop/web/utils/UUIDUtil.java
@@ -0,0 +1,29 @@
+package com.beta.prop.web.utils;
+
+import java.util.UUID;
+
+/**
+ * 
+ * @ClassName:  UUIDUtil   
+ * @Description:(UUID工具类)   
+ * @author: 邹尧
+ * @date:   2017年4月28日 上午11:51:50   
+ *     
+ * @Copyright: 2017 
+ *
+ */
+public class UUIDUtil {
+    
+    /**
+     * 
+     * @Title: getUUID   
+     * @Description: TODO(获取到UUID去除了横线)   
+     * @param: @return      
+     * @return: String      
+     * @throws
+     */
+    public  static  String  getUUID(){
+        String  uuid  =  UUID.randomUUID().toString().replaceAll("-", "");
+        return uuid;
+    }
+}
