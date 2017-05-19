927f5388befd0e43459c5b6fe8b02a841a17a8c2
betaIsMars <13889146832@163.com>
2017-05-19 11:01:17 +0800

    异步，锁，单例，HTTP 测试

--- src/main/java/com/beta/prop/Test.java
+++ src/main/java/com/beta/prop/Test.java
@@ -2,10 +2,30 @@ package com.beta.prop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test  {
-
+    public static CopyOnWriteArrayList<String>  cowl =  new CopyOnWriteArrayList<>();
     private List<Integer>  dataList = new ArrayList<Integer>();
+    public static   Integer  m = new Integer(0)  ;
+    public static volatile long te = 0L;
+    public static AtomicInteger ai = new AtomicInteger(0);
+    
+
+    
+    public   int add(){
+        synchronized(Test.m){
+         
+            return ++m;
+        }
+        
+    }
+    static synchronized int getm(){
+        return ++m;
+    }
     int a = 0;
     public     void  getSynchronizedInfor(long data){
        
@@ -20,7 +40,38 @@ public class Test  {
         for(int re : dataList){
             ab.append(re+"-");
         }
+        
         System.out.println("数据："+ab.toString());
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.println(Integer.MAX_VALUE);
+        Executor  exe =  Executors.newCachedThreadPool();
+        for(int i = 0 ; i < 10001 ; i++){
+            
+            final int se = i;
+            exe.execute(new Runnable() {
+                
+                @Override
+                public void run() {
+                	Test t = new Test();
+                    int a = t.add();
+                    Test.cowl.addIfAbsent(a+"");
+                    // System.out.println("数据："+a);
+                    System.out.println("Count:"+Test.cowl.size());
+                }
+            });
+        }
+        
+        try {
+            Thread.sleep(9000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(Test.cowl.size());
         
     }
 }
